d57b7eb4c8075175ad08171297337797df7109cb
Germán Flores <gfloresmarin@gmail.com>
2020-03-30 01:15:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,16 @@ client.on('message', message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLocaleLowerCase();
 
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    try {
+
+        let commandFile = require(`./comandos/${cmd}.js`);
+        commandFile.run(client, message, args);
+
+    } catch (e) {
+        console.log(e.stack);
+    }
+
 })
