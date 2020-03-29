52f61ac6d5f37a53fc4453d5b245b0c2796bb24f
Germán Flores <gfloresmarin@gmail.com>
2020-03-29 16:50:04 +0200

    Modificado bot.js
    Modificado para añadir el prefijo y simplificado.

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const  client = new Discord.Client();
-
+const prefix = '!';
 client.on("ready", () => {
    console.log("Hola amigo!");
 
@@ -12,16 +12,23 @@ client.on("ready", () => {
        }
    });
 });
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+
+client.on('message', message => {
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLocaleLowerCase();
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    try {
+
+      let commandFile = require(`./comandos/${cmd}.js`);
+      commandFile.run(client, message, args);
+
+    } catch (e) {
+        console.log(e.stack);
+    }
 });
-client.on("message", (message) => {
-  if (message.content.startsWith("Viva España!")) {
-    message.channel.send("Viva!!");
-  }
-});
+
 client.on('guildMemberAdd', member => {
 	let memberTag = member.user.tag;
     member.guild.channels.get('480936541968138250').send("Hey <@" + member.id + ">, bienvenid@ a **EspañaGaming** <:tada:530052691020218368><:hugging:530052691020218368> !"); 
