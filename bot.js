f375917ce79b75d337172cfdaf4c1a47d83df8a8
Germán Flores <gfloresmarin@gmail.com>
2020-03-30 01:51:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-client.user.setPresence({ activity: {name: '¡Bienvenido a EspañaGaming!' }, status: 'dnd'});
 
 
 client.on('ready', () => console.log('Iniciado!'));
+
+client.user.setStatus('dnd');
 
 const prefix = '!';
 
