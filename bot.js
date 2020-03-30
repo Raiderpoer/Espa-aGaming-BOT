c4e51723afa2e5735f4381df20003ecc5ac97129
Germán Flores <gfloresmarin@gmail.com>
2020-03-30 02:06:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-client.user.setPresence({ activity: { name: 'test' }, status: 'online'});
 
 
 client.on('ready', () => console.log('Iniciado!'));
